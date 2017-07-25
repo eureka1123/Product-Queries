1d637a0ca7139affbc94f42355f9b16b836aeba5
eureka1123 <xl.elaine.guo@gmail.com>
2017-07-25 17:44:49 +0000

    adding username variable

--- create_data_structure.py
+++ create_data_structure.py
@@ -1,8 +1,9 @@
 from nltk.tag.stanford import StanfordPOSTagger
 import nltk
 
-_path_to_model = "/home/abishkarchhetri/Product-Queries/stanford_tagger_files/stanford-postagger-2017-06-09/models/english-bidirectional-distsim.tagger" 
-_path_to_jar = "/home/abishkarchhetri/Product-Queries/stanford_tagger_files/stanford-postagger-2017-06-09/stanford-postagger.jar"
+username = "xiaoluguo"
+_path_to_model = "/home/" + username+ "/Product-Queries/stanford_tagger_files/stanford-postagger-2017-06-09/models/english-bidirectional-distsim.tagger" 
+_path_to_jar = "/home/" + username+ "/Product-Queries/stanford_tagger_files/stanford-postagger-2017-06-09/stanford-postagger.jar"
 
 st = StanfordPOSTagger(model_filename=_path_to_model, path_to_jar=_path_to_jar)
 
