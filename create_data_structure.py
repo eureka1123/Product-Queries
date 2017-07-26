e08cd6410939f4ac971069c76e7ce44c86d4734e
eureka1123 <xl.elaine.guo@gmail.com>
2017-07-26 19:18:32 +0000

    capitalization function

--- create_data_structure.py
+++ create_data_structure.py
@@ -4,10 +4,11 @@ import re
 import requests
 import json
 
-url = "http://pcsync-01/shoppersstop.com/pcf_catalog.json"
+client = "shoppersstop.com"
+url = "http://pcsync-01/" + client + "/pcf_catalog.json"
 file_request = requests.get(url, stream = True)
 
-username = "abishkarchhetri"
+username = "xiaoluguo"
 _path_to_model = "/home/" + username+ "/Product-Queries/stanford_tagger_files/stanford-postagger-2017-06-09/models/english-bidirectional-distsim.tagger" 
 _path_to_jar = "/home/" + username+ "/Product-Queries/stanford_tagger_files/stanford-postagger-2017-06-09/stanford-postagger.jar"
 
@@ -20,14 +21,20 @@ def lazy_json_read_line(line):
     except:
         return "error"
 
-f = open('tpdb.txt', 'w').close()
-f = open("tpdb.txt", "a")
+f = open(client + "_tpdb.txt", "w").close()
+f = open(client + "_tpdb.txt", "a")
 
 for line in file_request.iter_lines():
     f.write(lazy_json_read_line(line))
     f.write("\n")
 
 f.close()
+
+def check_not_all_capitalized(tokenized):
+    for word in tokenized:
+        if word.isupper() or word.islower():
+            return True
+    return False
 
 def create_data_structure(long_string):
     st = StanfordPOSTagger(model_filename=_path_to_model, path_to_jar=_path_to_jar)
@@ -37,24 +44,25 @@ def create_data_structure(long_string):
     print("sentences",sentences)    
     for sentence_index in range(len(sentences)): 
         tokenized = nltk.word_tokenize(sentences[sentence_index])
-    	tagged = st.tag(tokenized)
-	sentences_with_tag.append(tagged)
-	
-	for word_index in range(len(tagged)):
-	    word = tagged[word_index][0]
+	if check_not_all_capitalized(tokenized):
+	    tagged = st.tag(tokenized)
+	    sentences_with_tag.append(tagged)
+	    
+	    for word_index in range(len(tagged)):
+		word = tagged[word_index][0]
 
-	    if word not in main_data_structure:
-		main_data_structure[word] = {}
-		if sentence_index not in main_data_structure[word]:
-		    main_data_structure[word][sentence_index] =  [word_index]
+		if word not in main_data_structure:
+		    main_data_structure[word] = {}
+		    if sentence_index not in main_data_structure[word]:
+			main_data_structure[word][sentence_index] =  [word_index]
+		    else:
+			main_data_structure[word][sentence_index].append(word_index)
 		else:
-		    main_data_structure[word][sentence_index].append(word_index)
-	    else:
-		#main_data_structure[word].append((sentence_index, word_index))
-		if sentence_index not in main_data_structure[word]:
-                    main_data_structure[word][sentence_index] =  [word_index]
-                else:
-                    main_data_structure[word][sentence_index].append(word_index)
+		    #main_data_structure[word].append((sentence_index, word_index))
+		    if sentence_index not in main_data_structure[word]:
+			main_data_structure[word][sentence_index] =  [word_index]
+		    else:
+			main_data_structure[word][sentence_index].append(word_index)
 
     return (main_data_structure, sentences_with_tag)
 
