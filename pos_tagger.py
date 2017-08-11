c8b6ef8350a6a017301184a220ca08519fd6e5be
eureka1123 <xl.elaine.guo@gmail.com>
2017-08-11 14:07:49 +0000

    pos_tagger bug fixes

--- pos_tagger.py
+++ pos_tagger.py
@@ -3,11 +3,11 @@ from nltk import word_tokenize
 import re
 import ast
 
-username = "abishkarchhetri"
+username = "xiaoluguo"
 client = "shoppersstop.com"
 _path_to_model = "/home/" + username+ "/Product-Queries/stanford_tagger_files/stanford-postagger-2017-06-09/models/english-bidirectional-distsim.tagger"
 _path_to_jar = "/home/" + username+ "/Product-Queries/stanford_tagger_files/stanford-postagger-2017-06-09/stanford-postagger.jar"
-root = "/home/" + username +"/Product-Queries/"
+root = "/home/" + username +"/"
 tpdb_file = root + client + "_tpdb.txt"
 dict_file  = root + client + "_dict.txt"
 st = StanfordPOSTagger(model_filename=_path_to_model, path_to_jar=_path_to_jar)
@@ -28,12 +28,9 @@ def count(TPP_list):
     TPP_to_freq = {}
     TPP_without_distance = map(lambda x : (x[0], x[1]), TPP_list)
     for TPP in TPP_without_distance:
-	TPP_in_dict = TPP_to_freq.setdefault(TPP, 0)
-        TPP_in_dict += 1
+        TPP_to_freq.setdefault(TPP, 0)
+        TPP_to_freq[TPP] += 1
     return TPP_to_freq
-
-def get_max_from_TPP_dict(TPDict):
-    return max(TPDict, key=lambda key: TPDict[key])
 
 def score_function_single( word_query, tagged_sentences):
     word_POS_pairs = []
@@ -43,7 +40,7 @@ def score_function_single( word_query, tagged_sentences):
             word_POS_pairs.append(to_add)
 
     TPPDict = count(word_POS_pairs)
-    return get_max_from_TPP_dict(TPPDict)
+    return max(TPPDict, key=lambda key: TPPDict[key])
 
 def all_pairs(lst):
     if len(lst) == 1:
@@ -69,9 +66,8 @@ def get_TPP_and_freq(word_1_query,word_2_query, tagged_sentences, word_list): #q
     
     if len(word_1_index_list) != 0 and len(word_2_index_list) != 0 and len(common_sentences) == 0:
 	tag_1 = score_function_single(word_1_query, tagged_sentences)
-	tag_2 = score_function_single( word_2_query, tagged_sentences)
-	print(tag_1, tag_2)
-	return {(tag_1, tag_2):1}
+	tag_2 = score_function_single(word_2_query, tagged_sentences)
+	return [(tag_1, tag_2,2)]
 
     for sentence_index in common_sentences:
         for word_index in word_1_query[sentence_index]:
@@ -79,7 +75,8 @@ def get_TPP_and_freq(word_1_query,word_2_query, tagged_sentences, word_list): #q
                 distance = abs(word_index- word_2_index)
                 to_add = ((tagged_sentences[sentence_index][word_index][0].lower(),tagged_sentences[sentence_index][word_index][1]), (tagged_sentences[sentence_index][word_2_index][0].lower(),tagged_sentences[sentence_index][word_2_index][1]), distance)
                 word_POS_pairs.append(to_add)
-    
+   
+    return(word_POS_pairs) 
     #freq_dict = count(word_POS_pairs) #like freq dictionary returns {(t_1, POS_1): 1, (t_2, POS_2) : 3, ...}
     #return freq_dict
 
@@ -96,7 +93,6 @@ def get_max_norm_freq_from_TPP_dict(TPDict):
 def scoring_function_most_likelihood(word_pairs, list_freq_dict): #make it more efficient
     word_to_POS = {} #{word: {pos: <tag>, max_freq: <number>}}
     result = []
-    print("list_freq_dict",list_freq_dict)
     for TPDict in list_freq_dict: #TPDict = {(w1, t1): 3, ....}
         norm_freq, max_freq_TPP = get_max_norm_freq_from_TPP_dict(TPDict)
         for (word, tag) in max_freq_TPP:
@@ -115,14 +111,12 @@ def scoring_function_most_likelihood(word_pairs, list_freq_dict): #make it more 
 def get_tag_complete(search_words): #need to fix for single word search
 
     query = query_words(search_words, data) #returns a big dictionary of all positions of the words in the corpus {'long': {0: [2], 1: [12], 4: [4]}, 'sleeves': {2: [7]}}
-    list_of_freq_dict = []
+    list_of_freq_dict, list_of_freq_dict_A, list_of_freq_dict_B = [],[],[]
 
-    print(query.keys())
     search_words_temp = search_words[:]
     search_words = [x for x in search_words if query[x] is not None]
     other_words = [x for x in search_words_temp if query[x] is None] 
     
-    print("other_words", other_words) 
     result =[]
     if len(other_words)!=0:
         result.append(st.tag(other_words))
@@ -131,27 +125,53 @@ def get_tag_complete(search_words): #need to fix for single word search
         if query[search_words[0]] == None:
             result.append(st.tag([search_words[0]]))
             return result 
-        print("result", result)
 
         result.append(score_function_single( query[search_words[0]], tagged_sentences))
         return result
 
     word_pairs = all_pairs(search_words)
-    
+
     for word_1, word_2 in word_pairs:
-        
+        word_POS_pairs_A, word_POS_pairs_B = [],[]
 	word_POS_pairs = get_TPP_and_freq(query[word_1],query[word_2], tagged_sentences,[word_1,word_2]) #only gets consecutive pairs and their tag [(t_1, POS_1), (t_2, POS_2), ...]
-	freq_dict = count(word_POS_pairs)
-        if len(freq_dict) == 0:
-	    st_tag = st.tag([word_1, word_2])
-            list_of_freq_dict.append({st_tag:.5})
+        if len(word_pairs) ==1:
+            freq_dict = count(word_POS_pairs)
+	    if len(freq_dict) == 0:
+		st_tag = st.tag([word_1, word_2])
+		list_of_freq_dict.append({st_tag[0]:.5})
+		continue
+            list_of_freq_dict.append(freq_dict)
+
+	    term_POS_pairs = scoring_function_most_likelihood(word_pairs, list_of_freq_dict) #returns the tag of word pairs
+	    result.append(term_POS_pairs)
 	    continue
-
-
-	list_of_freq_dict.append(freq_dict)
-
-    term_POS_pairs = scoring_function_most_likelihood(word_pairs, list_of_freq_dict) #returns the tag of word pairs
-    result.append(term_POS_pairs)
+  
+        for x in word_POS_pairs:
+            if x[2]==1:
+                word_POS_pairs_A.append(x)
+            else: 
+                word_POS_pairs_B.append(x)
+        freq_dict_A = count(word_POS_pairs_A)
+        freq_dict_B = count(word_POS_pairs_B)
+        
+        if len(freq_dict_B) == 0 and len(freq_dict_A) == 0:
+            st_tag = st.tag([word_1, word_2])
+            list_of_freq_dict_B.append({st_tag[0]:.5})
+        
+        if len(freq_dict_A)!=0:
+            list_of_freq_dict_A.append(freq_dict_A)
+        if len(freq_dict_B)!=0:
+            list_of_freq_dict_B.append(freq_dict_B)      
+        
+    term_POS_pairs_A = []
+    term_POS_pairs_A_first = []
+    if len(list_of_freq_dict_A)!= 0:
+	term_POS_pairs_A = scoring_function_most_likelihood(word_pairs, list_of_freq_dict_A) #returns the tag of word pairs
+	result.append(term_POS_pairs_A)
+    results_first = [x[0] for x in result]
+    term_POS_pairs_A_first = [x[0] for x in term_POS_pairs_A if x[0] not in results_first]
+    term_POS_pairs_B = scoring_function_most_likelihood(word_pairs, list_of_freq_dict_B)
+    result.append([x for x in term_POS_pairs_B if x[0] not in term_POS_pairs_A_first and x[0] not in results_first])
     return result
 
 while True:
