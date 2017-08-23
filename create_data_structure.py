536b2efc0fef6604c4844097841fe4aa69033557
root <root@compute-master.c.ia-cloud-1249.internal>
2017-08-23 20:35:28 +0000

    cleaned up code

--- create_data_structure.py
+++ create_data_structure.py
@@ -4,7 +4,6 @@ import nltk
 import re
 import requests
 import json
-from multiprocessing.pool import ThreadPool as Pool
 import nlpnet
 
 client = "ebay.in"
@@ -12,47 +11,43 @@ url = "http://pcsync-01/" + client + "/pcf_catalog.json"
 file_request = requests.get(url, stream = True)
 
 username = "xiaoluguo"
+root1 = "/mnt/data/pos_ebay/"
 root = "/home/" + username + "/Product-Queries/"
 _path_to_model = root +  "stanford_tagger_files/stanford-postagger-2017-06-09/models/english-bidirectional-distsim.tagger" 
 _path_to_jar = root + "stanford_tagger_files/stanford-postagger-2017-06-09/stanford-postagger.jar"
-tpdb_file_pre = root + client + "_tpdb_pre.txt"
-tpdb_file_filter = root + client + "_tpdb_filter.txt"
-tpdb_file = root + client + "_tpdb.txt"
-dict_file  = root + client + "_dict.txt"
+tpdb_file_pre = root1 + client + "_tpdb_pre.txt"
+tpdb_file_filter = root1 + client + "_tpdb_filter.txt"
+tpdb_file = root1 + client + "_tpdb.txt"
+dict_file  = root1 + client + "_dict.txt"
 tag_list = ["JJ","JJR","JJS","NN","NNS","NNP","NNPS","VB","VBD","VBG","VBN","VBP","VBZ"]
-#pool = Pool(4)
 
-def lazy_json_read_line(line):
-    try:
-        line_json = json.loads(line[line.index("{"):-1])
-        line_description = line_json["description"]
-        return line_description
-    except:
-        return "error"
-
-f = open(tpdb_file_pre, "w").close()
-f = open(tpdb_file_pre, "a")
-
-counter = 1.0
-for line in file_request.iter_lines():
-    print(counter/len(file_request.iter_lines()))
-    counter+=1
-    try:
-        f.write(lazy_json_read_line(line).replace("\n",""))
-        f.write("\n")
-    except:
-        pass
-        #print(lazy_json_read_line(line))
-
-f.close()
-
-f = open(tpdb_file_pre, "r")
-set_input = set(f.read().split("\n"))
-f.close()
-
-f1 = open(tpdb_file_filter, "w")
-f1.write('\n'.join(set_input))
-f1.close()
+#def lazy_json_read_line(line):
+#    try:
+#        line_json = json.loads(line[line.index("{"):-1])
+#        line_description = line_json["description"]
+#        return line_description
+#    except:
+#        return "error"
+#
+#f = open(tpdb_file_pre, "w").close()
+#f = open(tpdb_file_pre, "a")
+#
+#for line in file_request.iter_lines():
+#    try:
+#        f.write(lazy_json_read_line(line).replace("\n",""))
+#        f.write("\n")
+#    except:
+#        pass
+#
+#f.close()
+#
+#f = open(tpdb_file_pre, "r")
+#set_input = set(f.read().split("\n"))
+#f.close()
+#
+#f1 = open(tpdb_file_filter, "w")
+#f1.write('\n'.join(set_input))
+#f1.close()
 
 def check_not_all_capitalized(tokenized):
     count = 0.0
@@ -65,7 +60,6 @@ def check_not_all_capitalized(tokenized):
 
 def check_has_verb(tagged):
     for word in tagged:
-        #print(word, word[1])
         if word[1] in ["VB","VBD","VBG","VBN","VBP","VBZ"]:
             return True
     return False
@@ -86,18 +80,15 @@ def create_data_structure(long_string):
     st = StanfordPOSTagger(model_filename=_path_to_model, path_to_jar=_path_to_jar)
     tagger = nlpnet.POSTagger("/mnt/static/nlpnet-en", language ="en")
     sentences = re.split("\. |\n", long_string)
-    #print("sentences",sentences)
     for sentence_index in range(len(sentences)):
-        #print("counter",counter)
         print(sentence_index*100.0/float(len(sentences)))
         tokenized = word_tokenize(sentences[sentence_index])
         if len(tokenized)!=0 and check_not_all_capitalized(tokenized):
-            tagged = st.tag(sentences[sentence_index])
+            tagged = tagger.tag(sentences[sentence_index])[0]
             if check_has_verb(tagged):
                 t.write(sentences[sentence_index])
                 t.write("\n")
                 sentences_with_tag.append(tagged)
-                #print(tagged)
                 f.write(str(tagged).strip('[]'))
                 f.write("\n")
 		for word_index in range(len(tagged)):
@@ -106,9 +97,7 @@ def create_data_structure(long_string):
 		    word_index_list = word_in_dict.setdefault(counter, [])
 		    word_index_list.append(word_index)
                 counter+=1 
-        if sentence_index%1000==0:
-            #print(sentences_with_tag)
-            #print(str(main_data_structure))
+        if sentence_index%10000==0:
             p = open(dict_file, "w")
 	    p.write(str(main_data_structure))
             p.close()
@@ -119,7 +108,6 @@ with open(tpdb_file_filter) as myfile:
 
 sample_string = "Some things never change. Do they? never"
 main_data_structure, sentences_with_tag = create_data_structure(corpus)
-#T = pool.map(create_data_structure(corpus))
 p = open(dict_file, "w")
 p.write(str(main_data_structure))
 p.close()
